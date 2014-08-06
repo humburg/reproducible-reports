c0f8f9ac9cbc7eafc19ab93734d6247344ac6f9d
Peter Humburg <humburg@well.ox.ac.uk>
2014-08-06 14:06:20 +0100

    added javascript to html5 output to align equations with numbers.

---
+++ include/equation.js
@@ -0,0 +1,26 @@
+<script>
+function css( element, property ) {
+    return window.getComputedStyle( element, null ).getPropertyValue( property );
+}
+
+function shiftImg(){
+	var images = document.querySelectorAll("div.equation img");
+
+	//Iterates through each of the images
+	for (var i = 0; i < images.length; i++) {
+	    //Sets the images top margin to the half of the height of the image
+		var offset = - images[i].height / 2;
+		var offset2 = Number(css(images[i].parentNode, "font-size").replace(/[^\d\.\-]/g, ''));
+		offset = offset - offset2 / 2;
+	    images[i].style.marginTop = offset.toString().concat("px");
+	    images[i].parentNode.style.marginBottom = images[i].style.marginTop; 
+	    images[i].parentNode.style.marginTop = (-offset).toString().concat("px");
+	}
+}
+if(window.addEventListener){
+    window.addEventListener('load',shiftImg,false); //W3C
+}
+else{
+    window.attachEvent('onload',shiftImg); //IE
+}
+</script>
