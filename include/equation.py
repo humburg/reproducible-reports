5a8e1d3cbb075a5f8c4e7233e574263a7c2de9ac
Peter Humburg <humburg@well.ox.ac.uk>
2014-08-08 11:55:12 +0100

    html support

--- include/equation.py
+++ include/equation.py
@@ -14,7 +14,7 @@ characters present in the equations ('&') are honoured. Equations
 without predifined alignment are aligned on the first '='.
 """
 
-from pandocfilters import toJSONFilter, RawBlock, Div
+from pandocfilters import toJSONFilter, RawBlock, Div, Math
 import re
 import sys
 
@@ -22,10 +22,11 @@ import sys
 def latex(x):
   return RawBlock('latex',x)
 
-def debug(key, value, id):
-    print >> sys.stderr, id
-    print >> sys.stderr, "  key: " + str(key)
-    print >> sys.stderr, "    value: " + str(value)
+def html(x):
+    return RawBlock('html', x)
+
+def DisplayMath(x):
+    return { u'c': [{u'c': [{u'c': [], u't': u'DisplayMath'}, x], u't': u'Math'}], u't': u'Plain'} 
 
 def iter_flatten(iterable):
   it = iter(iterable)
@@ -47,10 +48,34 @@ def getMath(x):
             return getMath(x['c'])
 
 def alignLatexMath(x):
+    global relSymbol
+    relPattern = '|'.join(relSymbol)
     if re.search(r'[^\\]&', x) is None:
-         idx = re.search('=', x).start()
+         idx = re.search(relPattern, x).start()
          return x[:idx] + '&' + x[idx:]
-    return x    
+    return x
+
+def alignHtmlMath(x):
+    global relSymbol
+    relPattern = '|'.join(relSymbol)
+    align = re.search(r'[^\\]&', x) 
+    if align is not None:
+        idx = align.start()
+        skip = 1
+    else:
+        idx = re.search(relPattern, x).start()
+        skip = 0
+    return [x[:idx+skip], x[idx+2*skip:idx+2*skip+1], x[idx+2*skip+1:]]
+    
+
+relSymbol = ['\\leq', '\\geq', '\\equiv', '\\models', '\\prec', '\\succ', '\\sim'
+             '\\perp', '\\preceq', '\\succeq', '\\simeq', '\\mid', '\\ll', '\\gg',
+             '\\asymp', '\\parallel', '\\subset', '\\supset', '\\approx', '\\bowtie',
+             '\\subseteq', '\\supseteq', '\\cong', '\\Join', '\\sqsubset', '\\sqsupset',
+             '\\neq', '\\smile', '\\sqsubseteq', '\\sqsupseteq', '\\doteq', '\\frown',
+             '\\in', '\\ni', '\\propto', '=', '\\vdash', '\\dashv', '<', '>']
+
+eqCount = 0
 
 def equation(key, value, format, meta):
     if key == 'Div':
@@ -58,7 +83,8 @@ def equation(key, value, format, meta):
         if 'equation' in classes:
             math = iter_flatten([ getMath(contents)])
             math = [x for x in math if x is not None]
-            label = '' 
+            label = ''
+            global eqCount 
             if format == 'latex':
                 type = 'equation'
                 if len(math) > 1:
@@ -71,6 +97,31 @@ def equation(key, value, format, meta):
                 return [latex('\\begin{' + type + '}' + label + "\n" + \
                               "\\\\\n".join(math) + \
                               "\n" + '\\end{' + type + '}')]
+            if format == 'html' or format == 'html5':
+                math = [alignHtmlMath(x) for x in math]
+                if ident != '':
+                    label = 'id=\"' + ident + '\" '
+                head = [html('<table ' + label + 'class=\"' + ' '.join(classes) + '\" ' + \
+                    ' '.join(kvs) + '>' + "\n")]
+                tail = [html('</table>' + "\n")]
+                body = [html('<tbody>' + "\n")]
+                for eq in math:
+                    eqCount = eqCount + 1
+                    body = body + [html('<tr>' + "\n")] + \
+                        [html(' <td class=\"eq_left\"> ')] + \
+                        [DisplayMath(eq[0])] + \
+                        [html(' </td>' + "\n")] + \
+                        [html(' <td class=\"eq_centre\">')] + \
+                        [DisplayMath(eq[1])] + \
+                        [html(' </td>' + "\n")] + \
+                        [html(' <td class=\"eq_right\"> ')] + \
+                        [DisplayMath(eq[2])] + \
+                        [html(' </td>' + "\n")] + \
+                        [html(' <td class=\"eq_number\"> (' + str(eqCount) + ') </td>')] + \
+                        [html('</tr>' + "\n")]
+                body = body + [html('</tbody>' + "\n")]
+                return head + body + tail
+                
         
 if __name__ == '__main__':
     toJSONFilter(equation)
